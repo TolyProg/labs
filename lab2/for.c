09774380fcc857837d9a0702c040ecc30930f83b
tolyprog <tolyprog@gmail.com>
2024-09-30 11:18:29 +0500

    add extra NL for images

--- lab2/for.c
+++ lab2/for.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-float f(float x) {
-    return 0 <= x && x <= 1 ?
-      cos(x)*exp(sqrt(pow(-x,2))) :
-      log(x+1)-sqrt(4-pow(x,2));
+float f(float x, float h) {
+    const float eps = h/2;
+    if(x > 0.0f - eps && x <= 1.1f + eps)
+      return cos(x)*exp(sqrt(pow(-x,2)));
+    else if(x >= 1 - eps && x <= 2 + eps)
+      return log(x+1)-sqrt(4-pow(x,2));
+    return 0.0f;
 }
 
 int main() {
     float h;
     printf("введите h> "); scanf("%f", &h);
     printf("x\t\tf(x)\n");
-    for(float x = 0; x <= 1.0f; x += h)
+    for(float x = 0; x <= 2.0f; x += h)
         printf("%f\t%f\n", x+h, f(x));
     return 0;
 }
--- lab2/while.c
+++ lab2/while.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-float f(float x) {
-    return 0 <= x && x <= 1 ?
-      cos(x)*exp(sqrt(pow(-x,2))) :
-      log(x+1)-sqrt(4-pow(x,2));
+float f(float x, float h) {
+    const float eps = h/2;
+    if(x > 0.0f - eps && x <= 1.1f + eps)
+      return cos(x)*exp(sqrt(pow(-x,2)));
+    else if(x >= 1 - eps && x <= 2 + eps)
+      return log(x+1)-sqrt(4-pow(x,2));
+    return 0.0f;
 }
 
 int main() {
     float h, x = 0;
     printf("введите h> "); scanf("%f", &h);
     printf("x\t\tf(x)\n");
-    while(x <= 1.0f) {
-        printf("%f\t%f\n", x+h, f(x));
+    while(x <= 2.0f) {
+        printf("%f\t%f\n", x+h, f(x, h));
         x += h;
     }
     return 0;
