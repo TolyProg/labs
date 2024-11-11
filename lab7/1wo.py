26d9dda5097550bd62ba22dbeaf5813d2076e2fc
tolyprog <tolyprog@gmail.com>
2024-11-11 10:45:35 +0500

    hmm

--- lab7/1wo.py
+++ lab7/1wo.py
@@ -1,5 +1,27 @@
 from itertools import chain
-def unpack(x):
-    #TODO
+def unpack(n):
+    r = []
+    q = [n]
+    while len(q) > 0:
+        n = q[-1]
+        del q[-1]
+        print(r, '|||', n)
+        if type(n) == str or type(n) == int:
+            r.append(n)
+            if len(q) < 1: break
+            n = q[-1]
+            del q[-1]
+            continue
+        for x in n:
+            match type(x).__name__:
+                case 'list' | 'tuple' | 'set':
+                    print('lts:', list(x), q)
+                    q.extend(list(x))
+                case 'dict':
+                    q.extend(list(x.keys()))
+                    q.extend(list(x.values()))
+                case _:
+                    r.append(x)
+    return r
 
 print(unpack([None, [1, ({2, 3}, {'foo': 'bar'})]]))
