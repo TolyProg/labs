a93d29eebafc192c1b1bb9299715c4ba868d8caf
tolyprog <tolyprog@gmail.com>
2024-11-18 09:38:49 +0500

    validate input

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MIN(A, B) (A < B ? A : B)
+
+void durak() {
+    printf("сработала защита от дурака\n");
+    exit(1);
+}
+
+void w(int r) {
+    if(r <= 0) durak();
+}
 
 int main() {
     int a, b, c, x;
-    printf("a = "); scanf("%i", &a);
-    printf("b = "); scanf("%i", &b);
-    printf("c = "); scanf("%i", &c);
-    printf("x = "); scanf("%i", &x);
+    printf("a = "); w(scanf("%i", &a));
+    printf("b = "); w(scanf("%i", &b));
+    printf("c = "); w(scanf("%i", &c));
+    printf("x = "); w(scanf("%i", &x));
     int m = MIN(a, MIN(b, c));
     if(m <= x && m % 7 == 0) printf("case 1: %i\n", m);
     else {
-        if(a+b+c-m == 0) {
-            printf("сработала защита от дурака\n");
-            return 1;
-        }
+        if(a+b+c-m == 0) durak();
         printf("case 2: %f\n", (float)m/(a+b+c-m));
     }
     return 0;
