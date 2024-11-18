e7d43b4170430595572f3f83f853301d47530198
tolyprog <tolyprog@gmail.com>
2024-11-18 09:18:47 +0500

    up the difficulty level

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -2,37 +2,77 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fill(int n, int a[]) {
-    for (int i = 0; i < n; ++i)
-        a[i] = rand() % 101 - 50;
+// что называется "tagged union"
+typedef struct Data {
+    enum {INT, FLOAT, DOUBLE, VOIDP} tag; // и т.д.
+    union {
+        int i; float f; double d; void *voidp;
+    } d; // тоже можно добавить значений
+    // кстати, остальные значения можно писать/читать,
+    // даже не имея для них специального тега путём
+    // кастования указателя на пустоту
+} Data;
+
+// однонаправленный список
+typedef struct List {
+    Data data;
+    struct List *next;
+} List;
+
+// не очень эффективно, но это же просто лаба :)
+void list_append(List *l, const Data d) {
+    while(l->next != NULL)
+        l = l->next;
+    l = malloc(sizeof(List));
+    *l = (List){d, NULL};
 }
 
-int compute(int n, int A[n]) {
+Data list_get(List *l, int i) {
+    // если будет передан out-of-bounds индекс, то программа вылетит
+    while(i-- > 0)
+        l = l->next;
+    return l->data;
+}
+
+void fill(int n, List *l) {
+    for (int i = 0; i < n; ++i) {
+        l->next = malloc(sizeof(List));
+        *l->next = (List){(Data){.tag=INT, .d.i=rand() % 101 - 50}};
+        l = l->next;
+    }
+    l->next = NULL;
+}
+
+int compute(int n, List *l) {
     /* Найдём 3 максимума и их индексы */
     int M[3] = {0}, I[3];
+    List *p = l;
     for(int i = 0; i < n; ++i) {
-        if(A[i] > *M) {
-            *M = A[i];
+        if(p->data.d.i > *M) {
+            *M = p->data.d.i;
             *I = i;
         }
+        l = l->next;
     }
+    p = l;
     for(int i = 0; i < n; ++i) {
         if(i == *I) {
             ++i;
             if(i >= n) break;
         }
-        if(A[i] > 1[M]) {
-            *(1+M) = A[i];
+        if(p->data.d.i > 1[M]) {
+            *(1+M) = p->data.d.i;
             1[I] = i;
         }
     }
+    p = l;
     for(int i = 0; i < n; ++i) {
         if(i == *I || i == *(1+I)) {
             ++i;
             if(i >= n) break;
         }
-        if(i[A] > 2[M]) {
-            *(2+M) = A[i];
+        if(p->data.d.i > 2[M]) {
+            *(2+M) = p->data.d.i;
             2[I] = i;
         }
     }
@@ -41,12 +81,12 @@ int compute(int n, int A[n]) {
 
 int main() {
     srand(time(NULL));
+    List l; // выделяю на стеке (эффективнее, чем malloc)
     int n;
     printf("n -> ");
     scanf("%d", &n);
-    int A[n];
-    fill(n, A);
+    fill(n, &l);
 
-    printf("%i\n", compute(n, A));
+    printf("%i\n", compute(n, &l));
     return 0;
 }
