84ef349d40f02e8b05b286dc5b43228d19b66bb6
User <User@wl5047.surgu.ru>
2025-04-15 11:09:41 +0500

    hmm

--- lab13/main.py
+++ lab13/main.py
@@ -1,14 +1,16 @@
+import apackage as p
 from kivy.app import App
 from kivy.uix.widget import Widget
-
-
-class UI(Widget):
-    pass
-
+from kivy.uix.textinput import TextInput
 
 class Lab13App(App):
-    def build(self):
-        return UI()
+    def shapeI_cb(self, value):
+        print(value)
+    #def cb(self, value):
+    #    print('cb', value)
+    #def __init__(self):
+    #    self.shape.bind(cb)
+    pass
 
 
 if __name__ == '__main__':
