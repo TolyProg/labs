d6c2790eff1daee7bfb4194893b28cee9af2aa56
Anatoly Mikhaylov <tolyprog at gmail.com>
2025-04-28 22:14:28 +0500

    temp

--- lab13/src/lab13/__init__.py
+++ lab13/src/lab13/__init__.py
@@ -4,12 +4,12 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
 
-def test_cb():
-    print('test_cb()')
-
 class Lab13App(App):
     def build(self):
         self.title = 'lab13: Gadostlivaya ZEST Bred Mk.II Deluxe Edition'
+    def test_cb(self):
+        print('test_cb()')
+
 
 
 
