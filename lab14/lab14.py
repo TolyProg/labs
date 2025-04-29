5d0c7ba213741bfec2973e9ec7a4243d5ca6e507
User <User@wl5047.surgu.ru>
2025-04-29 11:18:08 +0500

    _a_bit_of_progress

--- lab14/lab14.py
+++ lab14/lab14.py
@@ -1,9 +1,51 @@
+from icecream import ic
 import sqlite3
+import os
 
+os.remove('tutorial.db')
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
 
-cur.execute("CREATE TABLE items(name, description, price, have)")
-cur.execute("CREATE TABLE orders(date, status, linked)")
-cur.execute("CREATE TABLE clients(name, address, contact_info)")
+cur.execute("""CREATE TABLE Items
+(
+    name TEXT PRIMARY KEY,
+    description TEXT,
+    price INTEGER,
+    have INTEGER
+)""")
+cur.execute("""CREATE TABLE Orders
+(
+    id INTEGER,
+    date DATE,
+    status TEXT,
+    product FOREIGN KEY,
+    num INTEGER,
+    client INTEGER
+    PRIMARY KEY (id,product,num)
+    FOREIGN KEY (client) REFERENCES Clients
+)""")
+cur.execute("""CREATE TABLE Clients
+(
+    name PRIMARY KEY,
+    address,
+    contact_info
+)""")
 
+il = [
+    ("Морковь", "Съедобная, может быть вкусная", 50, 10),
+    ("Кортофель", "Съедобный, можно есть, может быть вкусный", 20, 10),
+    ("Процессор", "Который под столом, нельзя есть", 20, 2000),
+    ("Внешняя видеокарта", "Утолщение на проводе", 15, 1000),
+    ("V2_1", "Странное подобие логики", 3_000_000, 1),
+]
+
+ol = [
+    (1, "01-01-72", "OK", "Морковь", 3, 1)
+]
+
+for i in il: cur.execute(f'INSERT INTO Items VALUES {i}')
+for i in ol: cur.execute(f'INSERT INTO Orders VALUES {i}')
+
+ic(cur.execute('SELECT * FROM Items'), cur.fetchall())
+
+ic(cur.execute('SELECT * FROM Orders'), cur.fetchall())
